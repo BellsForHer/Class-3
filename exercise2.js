d62f1345419c09fdbfb86e4537c2991f762c7b86
BellsForHer <arhall88@gmail.com>
2022-08-22 20:20:16 -0500

    Create exercise2.js

---
+++ exercise2.js
@@ -0,0 +1,23 @@
+let user [
+    {
+        name: 'Susan',
+        age: 32,
+        occupation: 'Lawyer',
+    }, {
+        name: 'Kyle',
+        age: 45,
+        occupation: 'Chef',
+    }, {
+        name: 'Phyllis',
+        age: 40,
+        occupation: 'Party Planner',
+    }
+];
+
+
+
+function displayUser (user) {
+    let userButton = document.getElementById("button");
+};
+
+
