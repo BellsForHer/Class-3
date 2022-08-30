bdb09b51fa4201610cff80efd240baae26cf48e4
BellsForHer <arhall88@gmail.com>
2022-08-29 19:25:21 -0500

    Create exercise3.js

---
+++ exercise3.js
@@ -0,0 +1,18 @@
+function keysAndValues(data) {
+    return [Object.keys(data), Object.values(data)];
+}
+
+console.log(keysAndValues({a:1, b:2, c:3}));
+
+function keysAndValues(data) {
+    return [Object.keys(data), Object.values(data)];
+    }
+
+console.log(keysAndValues({a:'Apple', b:'Microsoft', c: 'Google'}));
+
+function keysAndValues(data) {
+    return [Object.keys(data), Object.values(data)];
+}
+
+console.log(keysAndValues({key1: true, key2: false, key3: undefined}));
+
